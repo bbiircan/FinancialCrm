422ba9debef8385d191c726002d22d86da4f1f80
bbiircan <bircanbircan1963@gmail.com>
2025-01-18 19:46:47 +0300

    Settings Form

---
+++ FrmSettings.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace FinancialCrm
+{
+    partial class FrmSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnDashboardForm = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnExpensesForm = new System.Windows.Forms.Button();
+            this.btnPaymentsForm = new System.Windows.Forms.Button();
+            this.btnBanksForm = new System.Windows.Forms.Button();
+            this.btnCategoriesForm = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnChangePass = new System.Windows.Forms.Button();
+            this.lblConfirmNewPass = new System.Windows.Forms.Label();
+            this.txtConfirmNewPass = new System.Windows.Forms.TextBox();
+            this.lblNewPass = new System.Windows.Forms.Label();
+            this.txtNewPass = new System.Windows.Forms.TextBox();
+            this.lblCurrentPass = new System.Windows.Forms.Label();
+            this.txtCurrentPass = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(54)))), ((int)(((byte)(130)))));
+            this.panel1.Controls.Add(this.btnLogout);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btnDashboardForm);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.btnExpensesForm);
+            this.panel1.Controls.Add(this.btnPaymentsForm);
+            this.panel1.Controls.Add(this.btnBanksForm);
+            this.panel1.Controls.Add(this.btnCategoriesForm);
+            this.panel1.Location = new System.Drawing.Point(0, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 456);
+            this.panel1.TabIndex = 16;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(54)))), ((int)(((byte)(130)))));
+            this.btnLogout.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.Location = new System.Drawing.Point(19, 385);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(186, 44);
+            this.btnLogout.TabIndex = 7;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(54)))), ((int)(((byte)(130)))));
+            this.button2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(19, 335);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 44);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Settings";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // btnDashboardForm
+            // 
+            this.btnDashboardForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(54)))), ((int)(((byte)(130)))));
+            this.btnDashboardForm.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDashboardForm.ForeColor = System.Drawing.Color.White;
+            this.btnDashboardForm.Location = new System.Drawing.Point(19, 285);
+            this.btnDashboardForm.Name = "btnDashboardForm";
+            this.btnDashboardForm.Size = new System.Drawing.Size(186, 44);
+            this.btnDashboardForm.TabIndex = 5;
+            this.btnDashboardForm.Text = "Dashboard";
+            this.btnDashboardForm.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(54)))), ((int)(((byte)(130)))));
+            this.button4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(19, 235);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(186, 44);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Account Activities";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // btnExpensesForm
+            // 
+            this.btnExpensesForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(54)))), ((int)(((byte)(130)))));
+            this.btnExpensesForm.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnExpensesForm.ForeColor = System.Drawing.Color.White;
+            this.btnExpensesForm.Location = new System.Drawing.Point(19, 185);
+            this.btnExpensesForm.Name = "btnExpensesForm";
+            this.btnExpensesForm.Size = new System.Drawing.Size(186, 44);
+            this.btnExpensesForm.TabIndex = 3;
+            this.btnExpensesForm.Text = "Expenses";
+            this.btnExpensesForm.UseVisualStyleBackColor = false;
+            // 
+            // btnPaymentsForm
+            // 
+            this.btnPaymentsForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(54)))), ((int)(((byte)(130)))));
+            this.btnPaymentsForm.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnPaymentsForm.ForeColor = System.Drawing.Color.White;
+            this.btnPaymentsForm.Location = new System.Drawing.Point(19, 135);
+            this.btnPaymentsForm.Name = "btnPaymentsForm";
+            this.btnPaymentsForm.Size = new System.Drawing.Size(186, 44);
+            this.btnPaymentsForm.TabIndex = 2;
+            this.btnPaymentsForm.Text = "Invoices";
+            this.btnPaymentsForm.UseVisualStyleBackColor = false;
+            // 
+            // btnBanksForm
+            // 
+            this.btnBanksForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(54)))), ((int)(((byte)(130)))));
+            this.btnBanksForm.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBanksForm.ForeColor = System.Drawing.Color.White;
+            this.btnBanksForm.Location = new System.Drawing.Point(19, 85);
+            this.btnBanksForm.Name = "btnBanksForm";
+            this.btnBanksForm.Size = new System.Drawing.Size(186, 44);
+            this.btnBanksForm.TabIndex = 1;
+            this.btnBanksForm.Text = "Banks";
+            this.btnBanksForm.UseVisualStyleBackColor = false;
+            // 
+            // btnCategoriesForm
+            // 
+            this.btnCategoriesForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(54)))), ((int)(((byte)(130)))));
+            this.btnCategoriesForm.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCategoriesForm.ForeColor = System.Drawing.Color.White;
+            this.btnCategoriesForm.Location = new System.Drawing.Point(19, 35);
+            this.btnCategoriesForm.Name = "btnCategoriesForm";
+            this.btnCategoriesForm.Size = new System.Drawing.Size(186, 44);
+            this.btnCategoriesForm.TabIndex = 0;
+            this.btnCategoriesForm.Text = "Categories";
+            this.btnCategoriesForm.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(106)))), ((int)(((byte)(186)))));
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(-1, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(847, 41);
+            this.panel2.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Settings Form";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel3.Controls.Add(this.btnChangePass);
+            this.panel3.Controls.Add(this.lblConfirmNewPass);
+            this.panel3.Controls.Add(this.txtConfirmNewPass);
+            this.panel3.Controls.Add(this.lblNewPass);
+            this.panel3.Controls.Add(this.txtNewPass);
+            this.panel3.Controls.Add(this.lblCurrentPass);
+            this.panel3.Controls.Add(this.txtCurrentPass);
+            this.panel3.Location = new System.Drawing.Point(291, 128);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(430, 281);
+            this.panel3.TabIndex = 18;
+            // 
+            // btnChangePass
+            // 
+            this.btnChangePass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(106)))), ((int)(((byte)(186)))));
+            this.btnChangePass.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnChangePass.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnChangePass.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnChangePass.Location = new System.Drawing.Point(262, 215);
+            this.btnChangePass.Name = "btnChangePass";
+            this.btnChangePass.Size = new System.Drawing.Size(110, 38);
+            this.btnChangePass.TabIndex = 6;
+            this.btnChangePass.Text = "Change";
+            this.btnChangePass.UseVisualStyleBackColor = false;
+            this.btnChangePass.Click += new System.EventHandler(this.btnChangePass_Click);
+            // 
+            // lblConfirmNewPass
+            // 
+            this.lblConfirmNewPass.AutoSize = true;
+            this.lblConfirmNewPass.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblConfirmNewPass.Location = new System.Drawing.Point(38, 170);
+            this.lblConfirmNewPass.Name = "lblConfirmNewPass";
+            this.lblConfirmNewPass.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblConfirmNewPass.Size = new System.Drawing.Size(203, 24);
+            this.lblConfirmNewPass.TabIndex = 5;
+            this.lblConfirmNewPass.Text = "Confirm New Password";
+            // 
+            // txtConfirmNewPass
+            // 
+            this.txtConfirmNewPass.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtConfirmNewPass.Location = new System.Drawing.Point(247, 173);
+            this.txtConfirmNewPass.Name = "txtConfirmNewPass";
+            this.txtConfirmNewPass.Size = new System.Drawing.Size(140, 28);
+            this.txtConfirmNewPass.TabIndex = 4;
+            // 
+            // lblNewPass
+            // 
+            this.lblNewPass.AutoSize = true;
+            this.lblNewPass.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNewPass.Location = new System.Drawing.Point(110, 128);
+            this.lblNewPass.Name = "lblNewPass";
+            this.lblNewPass.Size = new System.Drawing.Size(131, 24);
+            this.lblNewPass.TabIndex = 3;
+            this.lblNewPass.Text = "New Password";
+            // 
+            // txtNewPass
+            // 
+            this.txtNewPass.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNewPass.Location = new System.Drawing.Point(247, 128);
+            this.txtNewPass.Name = "txtNewPass";
+            this.txtNewPass.Size = new System.Drawing.Size(140, 28);
+            this.txtNewPass.TabIndex = 2;
+            // 
+            // lblCurrentPass
+            // 
+            this.lblCurrentPass.AutoSize = true;
+            this.lblCurrentPass.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCurrentPass.Location = new System.Drawing.Point(83, 83);
+            this.lblCurrentPass.Name = "lblCurrentPass";
+            this.lblCurrentPass.Size = new System.Drawing.Size(158, 24);
+            this.lblCurrentPass.TabIndex = 1;
+            this.lblCurrentPass.Text = "Current Password";
+            // 
+            // txtCurrentPass
+            // 
+            this.txtCurrentPass.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCurrentPass.Location = new System.Drawing.Point(247, 83);
+            this.txtCurrentPass.Name = "txtCurrentPass";
+            this.txtCurrentPass.Size = new System.Drawing.Size(140, 28);
+            this.txtCurrentPass.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(54)))), ((int)(((byte)(130)))));
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Location = new System.Drawing.Point(291, 128);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(430, 41);
+            this.panel4.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(131, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Change Password";
+            // 
+            // FrmSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 484);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmSettings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmSettings";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnDashboardForm;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnExpensesForm;
+        private System.Windows.Forms.Button btnPaymentsForm;
+        private System.Windows.Forms.Button btnBanksForm;
+        private System.Windows.Forms.Button btnCategoriesForm;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblCurrentPass;
+        private System.Windows.Forms.TextBox txtCurrentPass;
+        private System.Windows.Forms.Label lblConfirmNewPass;
+        private System.Windows.Forms.TextBox txtConfirmNewPass;
+        private System.Windows.Forms.Label lblNewPass;
+        private System.Windows.Forms.TextBox txtNewPass;
+        private System.Windows.Forms.Button btnChangePass;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ FrmSettings.cs
@@ -0,0 +1,50 @@
+﻿using FinancialCrm.Models;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FinancialCrm
+{
+    public partial class FrmSettings : Form
+    {
+        public FrmSettings()
+        {
+            InitializeComponent();
+        }
+        FinancialCrmDbEntities db = new FinancialCrmDbEntities();
+        private void btnChangePass_Click(object sender, EventArgs e)
+        {
+            string currentPass = txtCurrentPass.Text;
+            string newPass = txtNewPass.Text;
+            string confirmNewPass = txtConfirmNewPass.Text;
+
+            if (string.IsNullOrEmpty(newPass) || string.IsNullOrEmpty(confirmNewPass))
+            {
+                MessageBox.Show("Fill in the field for the new password and to confirm the password.");
+                return;
+            }
+
+            if (newPass != confirmNewPass)
+            {
+                MessageBox.Show("The passwords do not match.");
+                return;
+            }
+
+            var user = db.Users.FirstOrDefault(u => u.Password == currentPass);
+
+            if (user == null)
+            {
+                MessageBox.Show("The current password is incorrect.");
+                return;
+            }
+
+            user.Password = newPass;
+            db.SaveChanges();
+            MessageBox.Show("The password has been successfully updated.");
+
+        }
+    }
+}
+
+
+
